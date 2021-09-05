f4211a42304324a3108b3a83578647be7111b38c
Lavanya M K <lavanya@argoid.com>
2021-09-05 11:52:49 +0530

    feaviz manager

--- feaviz_manager/src/manager/data_manager.py
+++ feaviz_manager/src/manager/data_manager.py
@@ -1,7 +1,7 @@
 from src.data_processing.image_data_processing import read_and_process_data
 from src.data_processing.tabular_data_processing import read_and_process_tabular_data
 
-dashboards = {"tabular_dashboard": "http://localhost:8088/r/6", "image_dashboard": "http://localhost:8088/r/7"}
+dashboards = {"tabular_dashboard": ["http://localhost:8088/r/2", "http://localhost:8088/r/3"], "image_dashboard": "http://localhost:8088/r/7"}
 def create_tablular_data_visualization(data_path, keyspace):
     column_dtypes_list = read_and_process_tabular_data(data_path, keyspace)
     # need to add right value here
