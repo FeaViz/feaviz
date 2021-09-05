9fd6d24c95b0a3930869a5e2d28eca258a07fb0c
WingCode <thewinger94@gmail.com>
2021-09-05 07:37:11 +0400

    Added static dashboard urls

--- feaviz_manager/src/main.py
+++ feaviz_manager/src/main.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel
 from src.config import settings
 from src.manager.data_manager import create_image_data_visualization, create_tablular_data_visualization
 from fastapi.logger import logger
-from fastapi.responses import RedirectResponse
 
 
 class DataPath(BaseModel):
@@ -17,21 +16,17 @@ app = FastAPI(title='Feaviz - Feature visualization using Astra', debug=True)
 
 @app.post("/tables/")
 def create_tabular_feature_visualization(tabular_data_path: DataPath):
-    status = create_tablular_data_visualization(tabular_data_path.path, settings.keyspace)
-    if status>=1:
-        return RedirectResponse("http://localhost:8088/superset/dashboard/"+str(status))
-    else:
-        return RedirectResponse("http://localhost:8088/dashboard/list/")
+    dashboard_url = create_tablular_data_visualization(tabular_data_path.path, settings.keyspace)
+    return dashboard_url
+
 
 @app.post("/images")
 def create_image_feature_visualization(images_data_path: DataPath):
-    status = create_image_data_visualization(images_data_path.path, settings.keyspace, settings.http_server_url,
+    dashboard_url = create_image_data_visualization(images_data_path.path, settings.keyspace, settings.http_server_url,
                                     settings.model_label_path, settings.model_weights_path,
                                     settings.model_cfg_path)
-    if status >= 1:
-        return RedirectResponse("http://localhost:8088/superset/dashboard/"+str(status))
-    else:
-        return RedirectResponse("http://localhost:8088/dashboard/list/")
+    return dashboard_url
+
 
 @app.get("/")
 async def root():
--- feaviz_manager/src/manager/data_manager.py
+++ feaviz_manager/src/manager/data_manager.py
@@ -1,11 +1,11 @@
 from src.data_processing.image_data_processing import read_and_process_data
 from src.data_processing.tabular_data_processing import read_and_process_tabular_data
 
-
+dashboards = {"tabular_dashboard": "http://localhost:8088/r/6", "image_dashboard": "http://localhost:8088/r/7"}
 def create_tablular_data_visualization(data_path, keyspace):
     column_dtypes_list = read_and_process_tabular_data(data_path, keyspace)
     # need to add right value here
-    return 13
+    return dashboards["tabular_dashboard"]
 
 
 def create_image_data_visualization(images_data_path, keyspace, http_server_url,
@@ -15,4 +15,4 @@ def create_image_data_visualization(images_data_path, keyspace, http_server_url,
                               model_label_path, model_weights_path,
                               model_cfg_path)
     # need to add right value here
-    return 12
+    return dashboards["image_dashboard"]
